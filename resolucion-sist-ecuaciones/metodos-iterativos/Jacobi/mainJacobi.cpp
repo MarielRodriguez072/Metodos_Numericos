fa22cb68ccab913d904f0af673fa3a3e3e778e61
MarielRodriguez072 <rodriguezmariel072@gmail.com>
2026-09-01 10:28:46 -0300

    seguir con Jacobi. Primera versión de Interpolación

--- resolucion-sist-ecuaciones/metodos-iterativos/Jacobi/mainJacobi.cpp
+++ resolucion-sist-ecuaciones/metodos-iterativos/Jacobi/mainJacobi.cpp
@@ -12,20 +12,20 @@ int main(int argc, char const *argv[]){
   #include "../../lectura_escritura_datos.c"
   double b[filas];
   #include "../../lectura_escritura_datos_ideptes.c"
-  double epsilon = 0.001;
-  double e = 1;
+  double epsilon = 0.001; //tolerancia/error máximo permitido
+  double e = 1; //error calculado en cada iteracion
   double xv[filas];
   double xn[filas];
-  double s;
+  double s; //suma de los terminos que no son de la diagonal 
   int iteracion = 0;
-
+          //Comprobación de Diagonalmente Dominante
   for (int i  = 0; i < filas; i++){
     double suma = 0;
     for(int j = i+1; j < filas; j++)
       suma += m[i][j];
     cout << m[i][i] << " " << suma << endl;
     if(abs(m[i][i]) < abs(suma)){
-      cout << "la matriz no se puede resolver por este metodo" << endl;
+      cout << "la matriz no es diagonalmente dominante" << endl;
       return 0;
     }
   }
